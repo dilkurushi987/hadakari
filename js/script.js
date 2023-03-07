506240f64b00a4dbce53d81b82bcfb542d078fc6
dilkurushi987 <127222802+dilkurushi987@users.noreply.github.com>
2023-03-07 20:51:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
